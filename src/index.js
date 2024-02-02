19606b47f2d4fe2ee053a22ba0133504e8a50b71
Pedro Alarcon <pedroa54@icloud.com>
2024-02-01 21:00:10 -0600

    adds complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+
+const form = document.querySelector('#create-task-form')
+const description = document.querySelector('#new-task-description')
+const ul = document.querySelector(`#task`)
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const task = description.value;
+  const li = document.createElement('li');
+  li.innerText = task
+
+  const button = document.createElement('button')
+  button.innerText = 'x'
+
+button.addEventListener('click', (e) => {})
+li.appendChild(button)
+  ul.append(li)
+  e.target.reset()
+})
+
+
 });
